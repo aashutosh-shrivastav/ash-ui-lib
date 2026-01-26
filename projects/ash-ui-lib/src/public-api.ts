11bacc389501c972dc02acb4f6fa16782db22495
Aashutosh Shrivastav <ashuism7@gmail.com>
2026-01-26 23:12:38 +0530

    expose table component from the lib

--- projects/ash-ui-lib/src/public-api.ts
+++ projects/ash-ui-lib/src/public-api.ts
@@ -3,3 +3,18 @@
  */
 
 export * from './lib/ash-ui-lib';
+
+// AshTable exports
+export { AshTable } from './lib/ash-table/ash-table';
+export type { ColumnDef } from './lib/ash-table/models/column-def.model';
+export type { TableConfig } from './lib/ash-table/models/table-config.model';
+export type { 
+  SelectionMode,
+  FilterDescriptor,
+  SortDescriptor,
+  PageEvent,
+  ActionEvent,
+  SelectionEvent,
+  TableState
+} from './lib/ash-table/models/table-event.model';
+export { CellTemplateDirective } from './lib/ash-table/directives/cell-template.directive';
